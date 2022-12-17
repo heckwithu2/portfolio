b91015f79795fab4e4c92f7313c55f4f9f78ceba
heckwithu2 <jheck10@kent.edu>
2022-12-17 17:47:43 -0500

    https so far

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using portfolioApi.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 var serverVersion = new MySqlServerVersion(ServerVersion.AutoDetect(connectionString));
 
+if (builder.Environment.IsDevelopment()) {
 builder.Services.AddDbContext<portfolioApiContext>(
     dbContextOptions => dbContextOptions
         .UseMySql(connectionString, serverVersion)
@@ -15,7 +17,19 @@ builder.Services.AddDbContext<portfolioApiContext>(
         .LogTo(Console.WriteLine, LogLevel.Information)
         .EnableSensitiveDataLogging()
         .EnableDetailedErrors()
-);
+    );
+} else {
+    builder.Services.AddDbContext<portfolioApiContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+    );
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+        //configured in the appsettings.json
+        options.HttpsPort = 443;
+    });
+}
 
 
 // Add services to the container.
@@ -24,6 +38,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(60);
+    //options.ExcludedHosts.Add("example.com");
+    //options.ExcludedHosts.Add("www.example.com");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +54,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseExceptionHandler("/notdevelopment");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
